634cf734f3752e1f7d7a246d6c0c115a21b67f39
Ankit Kokane <97180942+thedudeontitan@users.noreply.github.com>
2023-07-13 02:10:04 +0530

    UI resposnsiveness for contact section
    Signed-off-by: Ankit Kokane <97180942+thedudeontitan@users.noreply.github.com>

--- app/(components)/Contact.tsx
+++ app/(components)/Contact.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Contact() {
   return (
-    <section className="min-h-screen flex">
+    <section className="min-h-screen flex mx-5 lg:mx-0">
       <div className="mt-20 flex justify-center w-full">
-        <div className="flex flex-col items-center justify-center">
-          <span className="text-[#66FCF1] text-3xl font-semibold opacity-80 lg:mb-8">
+        <div className="flex flex-col gap-10 lg:gap-0 items-center justify-center">
+          <span className="text-[#66FCF1] text-2xl lg:text-3xl font-semibold opacity-80 lg:mb-8">
             Liked My Work?
           </span>
-          <span className="text-5xl font-semibold opacity-90 text-[#f3f3f3] lg:mb-10">
-            Reach Out to me!
+          <span className="text-5xl font-semibold opacity-90 text-[#f3f3f3] lg:mb-10 text-center">
+            Reach Out to Me!
           </span>
-          <span className="lg:text-xl lg:w-[80vh] text-center font-medium opacity-75">
+          <span className="lg:text-xl text-lg lg:w-[80vh] text-center font-medium opacity-75">
             I am always looking for new opportunities to work with professionals
             and companies. I would also like to contirbute to opensource
             porjects providing real world value.
